74a8ebef24751cf379374b6e207f542ab3dead6d
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-10-04 16:26:53 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,22 @@
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+# installs nvm (Node Version Manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+
+# download and install Node.js (you may need to restart the terminal)
+nvm install 20
+
+# verifies the right Node.js version is in the environment
+node -v # should print `v20.17.0`
+
+# verifies the right npm version is in the environment
+npm -v # should print `10.8.2`
+
+git clone https://github.com/suidouble/sui_meta_miner.git
+cd sui_meta_miner/
+npm install
+
+screen -dmS sui bash -c "node mine.js --chain=mainnet --phrase='$phrase'"
