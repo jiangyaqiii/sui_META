d5400f759bb5cd2ccef11f384a6740138bd3dcde
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-10-05 12:25:32 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,6 +1,11 @@
 source ~/.bashrc
 cd ~
-source ~/.bashrc
+rm -f monit.sh
+rm -rf sui_meta_miner
+pkill monit
+git clone https://github.com/suidouble/sui_meta_miner.git
+cd sui_meta_miner
+npm install
 #监控screen脚本
 echo '#!/bin/bash
 while true
@@ -8,7 +13,7 @@ do
     if ! screen -list | grep -q "sui_meta"; then
         echo "Screen session not found, restarting..."
         cd ~/sui_meta_miner
-        screen -dmS sui_meta bash -c "node mine.js --chain=mainnet --phrase='$phrase'"
+        screen -dmS sui_meta bash -c "node mine.js --fomo --meta --chain=mainnet --phrase='suiprivkey1qrykv8zukh5zj624rqyzuwjur89n8mvqelqf7m2vg90safg2mw62qg8q7zn'"
     fi
     sleep 10  # 每隔10秒检查一次
 done' > monit.sh
